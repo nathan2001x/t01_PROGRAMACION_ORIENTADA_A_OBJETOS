3c31ca5d6b89a47874d86f72f494ef6bfb4a8954
Geovannyy <Geovannyy@Geovanny.home>
2021-07-14 19:29:11 -0500

    taller en clase

--- src/diagramas/de/flujo/Main4.java
+++ src/diagramas/de/flujo/Main4.java
@@ -6,17 +6,17 @@ public class Main4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method s
-				int n;
-				Scanner teclado=new Scanner(System.in);
-				System.out.println("Ingrese numero de filas ");
-				n=teclado.nextInt();
-				for(int f=n;f>=1;f=f-1) {
-					for(int a=f;a>=1;a=a-1) {
-						System.out.print("*");
-					}
-					System.out.println("");
-				
-	}
-	}
-}
+		System.out.println("Ingrese el número de filas:");
+		Scanner teclado = new Scanner(System.in);
+		int n = teclado.nextInt();
 
+		for (int f = 1; f <= n; f++) {
+
+			for (int a = n; a >= f; a--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
