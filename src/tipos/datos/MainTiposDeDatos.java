9736bb44a732fbe02bc9a53a3cdefe0a277609e3
Geovannyy <Geovannyy@Geovanny.home>
2021-07-21 18:49:25 -0500

    Tipos de datos

---
+++ src/tipos/datos/MainTiposDeDatos.java
@@ -0,0 +1,17 @@
+package tipos.datos;
+
+public class MainTiposDeDatos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		short cantidadEstudiantesUCE= 1000;
+		byte entradaCircuito=0;
+		int cantCuidadanos=10000;
+		long cantidadMundo=1000000000;
+		float sueldo = 10.56F;
+		double saldoBanco=100000.67;
+		char opcionMenu='c';
+		boolean casado=true;
+	}
+
+}
