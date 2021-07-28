95504768eeaecda968235fa2994803ec19ee8a8c
Geovannyy <Geovannyy@Geovanny.home>
2021-07-28 14:05:20 -0500

    correcion prueba

---
+++ src/pruebas/Prueba1.java
@@ -0,0 +1,33 @@
+package pruebas;
+
+import java.util.Scanner;
+
+public class Prueba1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner teclado=new Scanner (System.in);
+		int n,s=0,c=0;
+		float p=0f;
+		for( int i=1;i!=0;i++) {
+			System.out.println("Ingrese un numero positivo");
+			n=teclado.nextInt();
+			if(n==0) {
+				i=-1;
+			} else {
+				s=s+n;
+				p=s/i;
+				c=c+1;
+			}
+			
+		}
+		
+		System.out.println("La suma es: "+s);
+		System.out.println("El promdio es: "+p);
+		System.out.println("La cantidad es: " +c);
+	}
+
+}
+
+	
+
---
+++ src/pruebas/Prueba3ParImpar.java
@@ -0,0 +1,33 @@
+package pruebas;
+
+import java.util.Scanner;
+
+public class Prueba3ParImpar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner teclado=new Scanner (System.in);
+		int n,s=0, sp=0,si=0,ci=1;
+		float pi=0f;
+		for( int i=1;i!=-1;i++) {
+			System.out.println("Ingrese un numero positivo");
+			n=teclado.nextInt();
+			if(n==-1) {
+				i=-2;
+			} if(n%2==0) {
+				sp=sp+n;
+			} else {
+				si=si+n;
+				ci=ci+1;
+				pi=si/ci;
+			}
+			
+		}
+		
+		System.out.println("La suma de los pares es: "+sp);
+		System.out.println("El promedio de los impares es: "+pi);
+	}
+
+}
+
+
