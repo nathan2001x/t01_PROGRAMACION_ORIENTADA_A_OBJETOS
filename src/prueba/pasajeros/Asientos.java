91fc294538195f2d018476beac8ffd23c0b6c763
Nathan Velez <veleznathan64@gmail.com>
2021-08-30 20:54:35 -0500

    prueba

---
+++ src/prueba/pasajeros/Asientos.java
@@ -0,0 +1,160 @@
+package prueba.pasajeros;
+
+public class Asientos  {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			Adultos pedro = new Adultos();
+			pedro.setNombre("Pedro");
+			pedro.setApellido("Ramires");
+			pedro.setCodigoJubilacion(180);
+			pedro.setCodigoSeguro(001);
+			pedro.setEdad(60); //
+			
+			Adultos elias = new Adultos();
+			elias.setNombre("Elias");
+			elias.setApellido("Peres");
+			elias.setCodigoJubilacion(187);
+			elias.setCodigoSeguro(002);
+			elias.setEdad(57);
+			
+			Adultos pablo = new Adultos();
+			pablo.setNombre("Pablo");
+			pablo.setApellido("Lopes");
+			pablo.setCodigoJubilacion(197);
+			pablo.setCodigoSeguro(003);
+			pablo.setEdad(57);
+			
+			Adultos joan = new Adultos();
+			joan.setNombre("Joan");
+			joan.setApellido("Zamora");
+			joan.setCodigoJubilacion(187);
+			joan.setCodigoSeguro(004);
+			joan.setEdad(57);
+			
+			Adultos bryan = new Adultos();
+			bryan.setNombre("Bryan");
+			bryan.setApellido("Murillo");
+			bryan.setCodigoJubilacion(123);
+			bryan.setCodigoSeguro(005);
+			bryan.setEdad(54);
+			
+			Adultos andres = new Adultos();
+			andres.setNombre("Andres");
+			andres.setApellido("Jaramillo");
+			andres.setCodigoJubilacion(456);
+			andres.setCodigoSeguro(006);
+			andres.setEdad(56);
+			
+			Adultos valentina = new Adultos();
+			valentina.setNombre("Valentina");
+			valentina.setApellido("Velez");
+			valentina.setCodigoJubilacion(788);
+			valentina.setCodigoSeguro(007);
+			valentina.setEdad(69);
+			
+			Adultos jose = new Adultos();
+			jose.setNombre("Jose");
+			jose.setApellido("Ordoñez");
+			jose.setCodigoJubilacion(147);
+			jose.setCodigoSeguro(8);
+			jose.setEdad(23);
+			
+			Adultos maria = new Adultos();
+			maria.setNombre("Maria");
+			maria.setApellido("Velasco");
+			maria.setCodigoJubilacion(159);
+			maria.setCodigoSeguro(9);
+			maria.setEdad(68);
+			
+			Adultos andrea = new Adultos();
+			andrea.setNombre("Andrea");
+			andrea.setApellido("Perreira");
+			andrea.setCodigoJubilacion(852);
+			andrea.setCodigoSeguro(010);
+			andrea.setEdad(61);
+			
+			Adultos sebas = new Adultos();
+			sebas.setNombre("Sebas");
+			sebas.setApellido("Vallejo");
+			sebas.setCodigoJubilacion(963);
+			sebas.setCodigoSeguro(011);
+			sebas.setEdad(28);
+			
+			Adultos grace = new Adultos();
+			grace.setNombre("Grace");
+			grace.setApellido("Vallejo");
+			grace.setCodigoJubilacion(742);
+			grace.setCodigoSeguro(012);
+			grace.setEdad(30);
+			
+			Adultos jenny = new Adultos();
+			jenny.setNombre("Jenny");
+			jenny.setApellido("Gonzales");
+			jenny.setCodigoJubilacion(851);
+			jenny.setCodigoSeguro(013);
+			jenny.setEdad(61);
+			
+			Adolecentes juancho = new Adolecentes();
+			juancho.setApellido("Gonzales");
+			juancho.setEdad(16);
+			juancho.setNombre("Juancho");
+			
+			Adolecentes damian = new Adolecentes();
+			damian.setApellido("Chulca");
+			damian.setEdad(15);
+			damian.setNombre("Damian");
+			
+			Adolecentes cristian = new Adolecentes();
+			cristian.setApellido("Chulca");
+			cristian.setEdad(17);
+			cristian.setNombre("Cristian");
+			
+			Adolecentes roddy = new Adolecentes();
+			roddy.setApellido("Vasques");
+			roddy.setEdad(16);
+			roddy.setNombre("Roddy");
+			
+			Adolecentes edison = new Adolecentes();
+			edison.setApellido("Caiza");
+			edison.setEdad(16);
+			edison.setNombre("Edison");
+			
+			Adolecentes manuel = new Adolecentes();
+			manuel.setApellido("Cisto");
+			manuel.setEdad(14);
+			manuel.setNombre("Manuel");
+			
+			Adolecentes leonel = new Adolecentes();
+			leonel.setApellido("Jimenes");
+			leonel.setEdad(13);
+			leonel.setNombre("Leonel");
+	
+			Adolecentes listaAsientos [][]= new Adolecentes[4][5];
+			listaAsientos[0][0]=pedro;
+			listaAsientos[0][1]=elias;
+			listaAsientos[0][2]=pablo;
+			listaAsientos[0][3]=joan;
+			listaAsientos[0][4]=bryan;
+			listaAsientos[1][0]=jose;
+			listaAsientos[1][1]=sebas;
+			listaAsientos[1][2]=grace;
+			listaAsientos[1][3]=juancho;
+			listaAsientos[1][4]=damian;
+			listaAsientos[2][0]=cristian;
+			listaAsientos[2][1]=roddy;
+			listaAsientos[2][2]=edison;
+			listaAsientos[2][3]=manuel;
+			listaAsientos[2][4]=leonel;
+			listaAsientos[3][0]=andres;
+			listaAsientos[3][1]=valentina;
+			listaAsientos[3][2]=maria;
+			listaAsientos[3][3]=andrea;
+			listaAsientos[3][4]=henny;
+			
+			
+		
+	}
+
+}
